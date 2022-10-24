19043fa145e64cf75f3b2d7540731b89e8681f11
magadanuhak <32567230+magadanuhak@users.noreply.github.com>
2022-10-24 11:23:22 +0300

    Fixes
    refactor after code review

--- task12.php
+++ task12.php
@@ -1,25 +1,24 @@
 <?php 
 
 //task12//
-function checkEligibileVote() 
-{
+function getVoteEligibilityMessage(string $name, int $age): string 
+{ 
+    if(empty($name) || $age <= 0 || $age > 120){
+      return "Enter correct name and age";
+    } 
 
-$name = "Jhon";
-$age = 18;
-
-if ($age >= 18) {
-
-      echo  $name . "you are eligibile to vote";
-} else {
-
-      echo $name . "you are not eligibile to vote";
-
+   if ($age >= 18) {
+     return "{$name} you are eligibile to vote";
+   } 
+      
+   return "{$name} you are not eligibile to vote";
 }
 
 
-
-}
-checkEligibileVote();
+echo getVoteEligibilityMessage('John', 22);
+echo getVoteEligibilityMessage('Valentin', 122);
+echo getVoteEligibilityMessage('Michael', -1);
+echo getVoteEligibilityMessage('Lena', 18);
 
 ?>
 
