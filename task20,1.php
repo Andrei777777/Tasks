09a304910f2e9792dd8f365e45df2f22e197d1cd
Andrei <andreisirbu777@icloud.com>
2022-10-24 14:06:11 +0300

    V1.0

--- task20,1.php
+++ task20,1.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($_POST['submit'])) {
     if (!empty($_POST['check_list'])) {
-        // Counting number of checked checkboxes.
-        $checked_count = count($_POST['check_list']);
+       
+        $checkedCount = count($_POST['check_list']);
         $name = $_POST['username'];
-        echo $name . " 's favourite colors are " . $checked_count . " option(s): <br/>";
-        // Loop to store and display values of individual checked checkbox.
+        echo "{$name}  's favourite colors are  {$checkedCount}  option(s): <br/>";
+    
         foreach ($_POST['check_list'] as $selected) {
-            echo "<p>" . $selected . "</p>";
+            echo "<p> {$selected} </p>";
         }
     } else {
         echo "<b>Please Select Atleast One Option.</b>";
