65b2066a9698236aab3f74421edc3052518ed321
Andrei <andreisirbu777@icloud.com>
2022-10-28 17:25:43 +0300

    Main

--- Новая папка/task20,1.php
+++ Новая папка/task20,1.php
@@ -1,11 +1,13 @@
 <?php
+
 if (isset($_POST['submit'])) {
+    
     if (!empty($_POST['check_list'])) {
-       
+
         $checkedCount = count($_POST['check_list']);
         $name = $_POST['username'];
         echo "{$name}  's favourite colors are  {$checkedCount}  option(s): <br/>";
-    
+
         foreach ($_POST['check_list'] as $selected) {
             echo "<p> {$selected} </p>";
         }
